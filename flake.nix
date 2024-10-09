901ab9937b73025ebfeb1300aa9314c9f5230358
Felippe Alves <flipgm@gmail.com>
2024-10-09 09:21:58 -0300

    Changed to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Feli's NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
